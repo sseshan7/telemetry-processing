a5d7ff2ca342f63acb52a0479dda0e9c247f39e7
stevesdawg <shrivathsav.seshan@gmail.com>
2017-06-29 19:38:41 -0400

    test function added to ser_reader

--- serial_reader/ser_reader.py
+++ serial_reader/ser_reader.py
@@ -11,7 +11,19 @@ SERIAL_BAUDRATE = 115200
 SERIAL_TIMEOUT = 1 # Seconds
 #######################
 
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+    print("> {}".format(greeting))
 
+start_server = websockets.serve(hello, 'localhost', 2009)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+'''
 async def dump_to_socket():
     ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=SERIAL_TIMEOUT)
 
@@ -29,4 +41,6 @@ async def dump_to_socket():
 
 if __name__ == "__main__":
     # Socket should close itself after it's done
-    asyncio.get_event_loop().run_until_complete(dump_to_socket())
+    server = websockets.serve(callback, 'localhost', 2009)
+    #asyncio.get_event_loop().run_until_complete(dump_to_socket())
+'''
