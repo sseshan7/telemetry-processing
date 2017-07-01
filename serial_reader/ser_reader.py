20ec50891701063e1376d18debe18e12070aeb31
Roy Finkelberg <rfinkelberg3@gatech.edu>
2017-07-01 19:12:59 -0400

    Replace serial reader method with a coroutine

--- serial_reader/ser_reader.py
+++ serial_reader/ser_reader.py
@@ -1,9 +1,7 @@
-import serial
+import serial_asyncio
 import asyncio
 import websockets
 import json
-
-
 
 # Program Flow:
 # - continuously:
@@ -26,31 +24,36 @@ import json
 
 ### Configure Stuff ###
 WEBSOCKET = {
-'host' : 'localhost',
-'port' : 2009
+    'host': 'localhost',
+    'port': 2009
 }
 
 SERIAL_PORT = '/dev/tty.usbserial'
 SERIAL_BAUDRATE = 115200
-SERIAL_TIMEOUT = 1 # Seconds
+SERIAL_TIMEOUT = 1  # Seconds
 #######################
-ser = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUDRATE, timeout=SERIAL_TIMEOUT)
-
-subscriptions = set() # if we have a finite number, should be a dict
-data = dict() # might end up being a dict of pandas dataframes or something
-
-if __name__ == '__main__':
-    # probably read serial data here
-
-    start_server = websockets.serve(recieve_messages, WEBSOCKET['host'], WEBSOCKET['port'])
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.ensure_future(read_serial())
-    asyncio.get_event_loop().run_forever()
+# ser = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUDRATE, timeout=SERIAL_TIMEOUT)
+### Global Variables ###
+subscriptions = set()  # if we have a finite number, should be a dict
+data = dict()  # might end up being a dict of pandas dataframes or something
 
 
+class SerialReader(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        transport.serial.rts = False
+        transport.write(b'hello world\n')  # test write
+
+    def data_received(self, incoming):
+        # temporary, should instead parse and save data
+        print('received: ', incoming)
+
+    def connection_lost(self, exc):
+        print('connection lost')
+        self.transport.loop.stop()
 
 
-async def recieve_messages(websocket, path):
+async def receive_messages(websocket, path):
     still_reading = True
     while still_reading:
         # Assuming msgs are recieved as text frames, not binary frames
@@ -59,39 +62,44 @@ async def recieve_messages(websocket, path):
         # I'm assuming (maybe wrongly) that msgs are in the form
         # "history (element)" and so on for the other commands
         if message[0] == "history":
-            await handle_history(message[1])
-        # elif ...
+            await handle_history(element=message[1])
+            # elif ...
 
-async def read_serial(websocket, path):
-    global data
-    global ser
-
-    still_reading = True
-    # I'm worried this will halt the rest of the program despite being an async coroutine
-    while still_reading:
-        incoming = (ser.readline()).split()
-        acc_data = (incoming[0], incoming[1], incoming[2])
-        gyro_data = (incoming[3], incoming[4], incoming[5])
-        mag_data = (incoming[6], incoming[7], incoming[8])
-        # save this to data in some form
 
 async def update_subscribers(websocket, path):
     # foreach subscription in subscriptions, send live data
     global subscriptions
+
 
 async def handle_history(websocket, path, element):
     # send data as json object
     global data
     global subscriptions
 
+
 def handle_subscribe(element):
     # add subscription to subscriptions
     global subscriptions
+
 
 def handle_unsubscribe(element):
     # remove subscription from subscriptions
     global subscriptions
 
+
+if __name__ == '__main__':
+    # probably read serial data here
+    loop = asyncio.get_event_loop()
+    serial_coroutine = serial_asyncio.create_serial_connection(loop,
+                                                               SerialReader,
+                                                               SERIAL_PORT,
+                                                               SERIAL_BAUDRATE)
+
+    start_server = websockets.serve(receive_messages, WEBSOCKET['host'],
+                                    WEBSOCKET['port'])
+    loop.run_until_complete(start_server)
+    asyncio.ensure_future(serial_coroutine)
+    loop.run_forever()
 
 
 # async def dump_to_socket():
